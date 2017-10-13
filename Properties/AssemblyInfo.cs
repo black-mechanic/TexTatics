f180f29f39c698d1cb89d5d71f3b461a83299d97
Andrey <andreynuh@gmail.com>
2017-10-13 19:02:10 +0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TeXtatics")]
+[assembly: AssemblyDescription("Подсчет простой статистики текстового файла")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Garret")]
+[assembly: AssemblyProduct("TeXtatics")]
+[assembly: AssemblyCopyright("Copyright Nuh ©  2017")]
+[assembly: AssemblyTrademark("Garret")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("897fb0ab-02a8-4132-89da-1655697651c9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
